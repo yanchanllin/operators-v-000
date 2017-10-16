b4ab4592e710c5122283a7219676af1c1124ba2d
YanChan  Lin <yonalin1@gmail.com>
2017-10-16 16:17:17 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,11 @@
 def unsafe?(speed)
 if speed >60|| speed<40
 return true
-elsif speed >=40 && speed <=60 
+elsif speed >=40 && speed <=60
 	return false
- end 
+ end
 end
 
 def not_safe?(speed)
-	speed >=40 && speed <=60 ? false : true 
+	speed >=40 && speed <=60 ? false : true
 end
-	
-
-
